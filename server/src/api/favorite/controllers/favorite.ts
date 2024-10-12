c9b161c3081790777af4826eb2c48638b35653f4
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2024-10-12 16:00:59 -0500

    refactor controller and moved business login to service

--- server/src/api/favorite/controllers/favorite.ts
+++ server/src/api/favorite/controllers/favorite.ts
@@ -12,83 +12,15 @@ export default factories.createCoreController("api::favorite.favorite", {
     if (!user) return ctx.throw(401, "Unauthorized");
     const userId = user?.documentId;
 
-    const recipe = await strapi.service("api::recipe.recipe").findOne(recipeId);
-    if (!recipe) return ctx.throw(404, "Recipe not found");
-
-    console.log("##########################");
-    console.log(recipe);
-    console.log("##########################");
-
-    const favorites = await strapi
-      .documents("api::favorite.favorite")
-      .findMany({
-        filters: {
-          userId: userId,
-          recipeId: recipe.documentId,
-        },
-      });
-
-    let isFavorite = favorites.length > 0;
-
-    async function addRecipeLikeCount(recipeId: string) {
-      await strapi.service("api::recipe.recipe").update(recipeId, {
-        data: {
-          likes: recipe.likes + 1,
-        }
-      })  
-    }
-
-    async function removeRecipeLikeCount(recipeId: string) {
-      await strapi.service("api::recipe.recipe").update(recipeId, {
-        data: {
-          likes: recipe.likes - 1,
-        }
-      })  
-    }
-
-    if (!isFavorite) {
-      const newFavorite = await strapi
-        .documents("api::favorite.favorite")
-        .create({
-          data: {
-            userId: userId,
-            recipeId: recipeId,
-          },
-          status: "published",
-        });
-
-      await addRecipeLikeCount(recipeId); 
-      
-    } else {
-      await strapi.documents("api::favorite.favorite").delete({
-        documentId: favorites[0].documentId,
-      });
-
-      await removeRecipeLikeCount(recipeId);
-    }
-
-    // 1 check if favorite exists
-    // create favortite
-
-    // 2 cehck if the user liked it
-    // update to unlike
-
-    if (isFavorite) {
-      console.log("isFavorite");
-    } else {
-      console.log("not isFavorite");
-    }
-
-    // let favorite;
-
-    // if (true) favorite = await strapi.service("api::favorite.favorite").favorite(id, user.id);
-    // else favorite = await strapi.service("api::favorite.favorite").unfavorite(id, user.id);
+    const favoriteService = strapi.service("api::favorite.favorite");
+    const { isAlreadyFavorited, updatedRecipe } = await favoriteService.toggleFavorite(userId, recipeId);
 
     return (ctx.body = {
       data: {
         id: recipeId,
         user: userId,
-        isFavorite: isFavorite,
+        isFavorite: !isAlreadyFavorited,
+        likes: updatedRecipe.likes,
       },
     });
   },
--- server/src/api/favorite/services/favorite.ts
+++ server/src/api/favorite/services/favorite.ts
@@ -2,13 +2,50 @@
  * favorite service
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreService('api::favorite.favorite', {
-  async favorite(id, userId) {
-    console.log(id, userId);
-  },
-  async unfavorite(id, userId) {
-    console.log(id, userId);
+export default factories.createCoreService("api::favorite.favorite", {
+  async toggleFavorite(userId: string, recipeId: string) {
+    const recipe = await strapi.service("api::recipe.recipe").findOne(recipeId);
+    if (!recipe) throw new Error("Recipe not found");
+
+    const favorites = await strapi
+      .documents("api::favorite.favorite")
+      .findMany({
+        filters: {
+          userId: userId,
+          recipeId: recipe.documentId,
+        },
+      });
+
+    const isAlreadyFavorited = favorites.length > 0;
+
+    if (!isAlreadyFavorited) {
+      await strapi.documents("api::favorite.favorite").create({
+        data: {
+          userId: userId,
+          recipeId: recipeId,
+        },
+        status: "published",
+      });
+
+      recipe.likes += 1;
+    } else {
+      await strapi.documents("api::favorite.favorite").delete({
+        documentId: favorites[0].documentId,
+      });
+
+      recipe.likes -= 1;
+    }
+
+    const updatedRecipe = await strapi
+      .service("api::recipe.recipe")
+      .update(recipeId, {
+        data: {
+          likes: recipe.likes,
+        },
+      });
+
+    return { isAlreadyFavorited, updatedRecipe };
   },
 });
